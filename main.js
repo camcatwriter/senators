f082e7ebf1e60a65383d701f80c5f1d49476065e
camcatwriter <theothertotalbookworm@gmail.com>
2019-04-30 07:57:21 -0600

    fixed whole thing

--- main.js
+++ main.js
@@ -1,64 +1,124 @@
+// import { senators } from './senators.js'
+// import { representatives } from './representatives.js'
+
+// //filter examples
+// const maleSenators = representatives.filter((senator) => {
+//     return senator.gender === 'M'
+// })
+
+// console.log (maleSenators);
+
+// const femaleSenators = representatives.filter((senator) => {
+//     return senator.gender === 'F'
+// })
+
+// console.log (femaleSenators);
+
+// //map examples
+
+// const simpleReps = respresentatives.map(rep => {
+//     return {
+//         name: '${rep.first_name} ${rep.last_name}',
+//         party: rep.party
+//     }
+// })
+
+// const republicans = simpleReps.filter(rep => rep.party === "R")
+// const democrats = simpleReps.filter(rep => rep.party === "D")
+
+// console.log(republicans, democrats)
+
+// console.log(simpleReps)
+
+// //reduce examples
+
+// const testArray = [5,10,15,20,25,30,35,40]
+// const testReduce = testArray.reduce((acc,num) => {
+//     return acc + num
+// }, 0)
+
+// const missedVotes = senators.reduce((acc, senator) => {
+//     return acc + senator.missed_votes
+// }, 0)
+
+// const totalVotes = senators.reduce((acc, senator) => {
+//     return acc + senator.total_votes
+// }, 0)
+
+// console.log('Out of ${totalVotes} total votes in the senate, ${missedVotes} were missed')
+
+// //display logged information
+// console.log(testReduce, missedVotes, total_votes)
+
+// const senWithPics = senators.map(senator => {
+//     senator.imgURL = 'https://www.congress.gov/img/member/${senator.id.toLowerCase()}.jpg'
+//     return senator
+// })
+
+// //display pictures
+// let pictureDiv = document.querySelector('.container')
+
+// senWithPics.forEach(senator => {
+//     let senatorPic = document.createElement('img')
+//     let senatorFig = document.createElement('figure')
+//     let senatorCap = document.createElement('figcaption')
+//     senatorCap.textContent = `${senator.first_name} ${senator.last_name}`
+//     senatorPic.src = senator.imgURL
+//     senatorFig.appendChild(senatorPic)
+//     senatorFig.appendChild(senatorCap)
+//     pictureDiv.appendChild(senatorFig)
+// })
+
 import { senators } from './senators.js'
 import { representatives } from './representatives.js'
 
-//filter examples
-const maleSenators = representatives.filter((senator) => {
-    return senator.gender === 'M'
+// filter examples
+
+const democrats = representatives.filter(senator => {
+  return senator.party === 'D'
 })
 
-console.log (maleSenators);
+const republicans = representatives.filter(senator => senator.party === 'R')
 
-const femaleSenators = representatives.filter((senator) => {
-    return senator.gender === 'F'
+console.log(democrats, republicans)
+
+// map example - simplify the object returned from the map function by just creating two properties for each one
+
+const simpleReps = representatives.map(rep => {
+  return {
+    name: `${rep.first_name} ${rep.last_name}`,
+    facebook: rep.facebook_account,
+  }
 })
 
-console.log (femaleSenators);
+// reduce example
 
-//map examples
+const testArray = [5, 10, 15, 20, 25, 30, 35, 40]
 
-const simpleReps = respresentatives.map(rep => {
-    return {
-        name: '${rep.first_name} ${rep.last_name}',
-        party: rep.party
-    }
-})
-
-const republicans = simpleReps.filter(rep => rep.party === "R")
-const democrats = simpleReps.filter(rep => rep.party === "D")
-
-console.log(republicans, democrats)
-
-console.log(simpleReps)
-
-//reduce examples
-
-const testArray = [5,10,15,20,25,30,35,40]
-const testReduce = testArray.reduce((acc,num) => {
-    return acc + num
+const testResults = testArray.reduce((acc, num) => {
+  return acc + num
 }, 0)
 
-const missedVotes = senators.reduce((acc, senator) => {
-    return acc + senator.missed_votes
+const allVotes = representatives.reduce((acc, rep) => {
+  return acc + rep.total_votes
 }, 0)
 
-const totalVotes = senators.reduce((acc, senator) => {
-    return acc + senator.total_votes
+const allMissedVotes = representatives.reduce((acc, rep) => {
+  return acc + rep.missed_votes
 }, 0)
 
-console.log('Out of ${totalVotes} total votes in the senate, ${missedVotes} were missed')
-
-//display logged information
-console.log(testReduce, missedVotes, total_votes)
+// should provide some nice UI to show these results instead of just printing to the console
+console.log(testResults, allVotes, allMissedVotes)
 
 const senWithPics = senators.map(senator => {
-    senator.imgURL = 'https://www.congress.gov/img/member/${senator.id.toLowerCase()}.jpg'
-    return senator
+  senator.imgURL = `https://www.congress.gov/img/member/${senator.id.toLowerCase()}.jpg`
+  return senator
 })
 
-//display pictures
-let pictureDiv = document.querySelector('.container')
+// now set up some UI elements to display the senator pictures
+  let pictureDiv = document.querySelector('.container')
 
-senWithPics.forEach(senator => {
+  senWithPics.forEach(senator => {
     let senatorPic = document.createElement('img')
     let senatorFig = document.createElement('figure')
     let senatorCap = document.createElement('figcaption')
@@ -67,4 +127,4 @@ senWithPics.forEach(senator => {
     senatorFig.appendChild(senatorPic)
     senatorFig.appendChild(senatorCap)
     pictureDiv.appendChild(senatorFig)
-})
+  })
